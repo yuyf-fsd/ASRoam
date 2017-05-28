98d6014bb6584963f7a0312dc9bcede027c4599f
Yanfei Yu <yuyanfei@ifengyu.com>
2017-05-28 22:42:02 +0800

    Create porject

---
+++ ASRoam/ASRoam/VCs/FirstVC.swift
@@ -0,0 +1,26 @@
+//
+//  FirstVC.swift
+//  ATRoam
+//
+//  Created by Yanfei Yu on 2017/2/28.
+//  Copyright © 2017年 Ataluer. All rights reserved.
+//
+
+import UIKit
+//import Foundation
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
